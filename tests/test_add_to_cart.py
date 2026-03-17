646201842697bfc7bdc4ea34e76fa1470f2037d7
Ayush <ayushgadag09@gmail.com>
2026-03-17 22:38:30 +0530

    Removed Manual Login and Login happens automatically using fixture

--- tests/test_add_to_cart.py
+++ tests/test_add_to_cart.py
@@ -1,19 +1,15 @@
-from pages.login_page import LoginPage
 from pages.product_page import ProductPage
-from selenium.webdriver.common.by import By
+from pages.cart_page import CartPage
 
 
-def test_add_to_cart(driver):
+def test_add_to_cart(logged_in_user):
 
-    driver.get("https://www.saucedemo.com")
-
-    login = LoginPage(driver)
-    login.login("standard_user", "secret_sauce")
+    driver = logged_in_user
 
     product = ProductPage(driver)
     product.add_product_to_cart()
     product.open_cart()
 
-    cart_item = driver.find_element(By.CLASS_NAME, "inventory_item_name")
+    cart = CartPage(driver)
 
-    assert cart_item.is_displayed()
+    assert "Sauce Labs Backpack" in cart.is_product_in_cart()
