dc123e83f44a829d6a1fe338576b555382d6f170
Ayush <ayushgadag09@gmail.com>
2026-03-17 22:52:12 +0530

    add Auto login using fixture

--- conftest.py
+++ conftest.py
@@ -1,10 +1,19 @@
 import pytest
 from utils.driver_factory import get_driver
+from pages.login_page import LoginPage
 from datetime import datetime
 import os
 
 
+# ---------- DRIVER FIXTURE ----------
 @pytest.fixture
+def driver():
+    driver = get_driver()
+    yield driver
+    driver.quit()
+
+
+# ---------- LOGIN FIXTURE ----------
 @pytest.fixture
 def logged_in_user(driver):
     driver.get("https://www.saucedemo.com")
@@ -13,26 +22,22 @@ def logged_in_user(driver):
     return driver
 
 
+# ---------- SCREENSHOT ON FAILURE ----------
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item):
     outcome = yield
     rep = outcome.get_result()
 
-    # check if test failed
+    # Run only if test failed
     if rep.when == "call" and rep.failed:
 
-        # get driver instance from test
         driver = item.funcargs["driver"]
 
-        # create screenshots folder if not exists
         os.makedirs("screenshots", exist_ok=True)
 
-        # create timestamp for unique screenshot name
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-
         screenshot_name = f"screenshots/failure_{timestamp}.png"
 
-        # save screenshot
         driver.save_screenshot(screenshot_name)
 
         print(f"Screenshot saved: {screenshot_name}")
