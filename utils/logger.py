db34b37b3ac68749732238a5b1f55e3ebe9d540b
Ayush <ayushgadag09@gmail.com>
2026-03-18 22:42:23 +0530

    for Logging system added logger.py

---
+++ utils/logger.py
@@ -0,0 +1,16 @@
+import logging
+
+def get_logger():
+
+    logger = logging.getLogger("automation")
+
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        file_handler = logging.FileHandler("logs/test.log")
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    return logger
