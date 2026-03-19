f7542315712150517687af5f0cf01939144565cc
Ayush <ayushgadag09@gmail.com>
2026-03-19 19:41:35 +0530

    Add driver factory and headless execution support

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,8 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
-def get_driver():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+def get_driver(headless=False):
+
+    options = Options()
+
+    if headless:
+        options.add_argument("--headless=new")
+        options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
+
     driver.maximize_window()
     return driver
